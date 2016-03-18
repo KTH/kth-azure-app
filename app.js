c790461319073fd5766b1771ab3182839ea948df
Patric Jansson <paddy@kth.se>
2016-03-18 13:53:57 +0100

    componse

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('1337');
+  res.send(req.headers.host);
 });
 
 app.listen(3000, function () {
