8827f866ea945f8ec58aae2b1da77a1c373cd528
Patric Jansson <paddy@kth.se>
2017-08-31 12:44:05 +0200

    Basic integration test

---
+++ integration-tests/basic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+error() { printf "\n \033[0;31mERROR: $@\033[0;0m | $(date) \n"; }
+passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
+
+MONITOR_URL="http://web:3000/_monitor";
+PATTERN="APPLICATION_STATUS"
+
+RESPONSE=`curl -s --max-time 10 $MONITOR_URL`
+
+if [[ -z `echo $RESPONSE | grep $PATTERN` ]]; then
+    error "Monitor page '$MONITOR_URL' did not contain '$PATTERN'."
+    exit -1
+fi
+
+passed "Basic monitor test passed for '$MONITOR_URL'."
