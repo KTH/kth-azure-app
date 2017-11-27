2beffa95c29a7b23ed5b6044a0b7e9a85a89f815
Patric Jansson <paddy@kth.se>
2017-11-27 15:17:02 +0100

    testar integration test

--- tests/integration-tests/basic.sh
+++ tests/integration-tests/basic.sh
@@ -6,7 +6,9 @@ passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
 MONITOR_URL="http://web:3000/_monitor";
 PATTERN="APPLICATION_STATUS"
 
-RESPONSE=`curl -s -S --max-time 10 $MONITOR_URL`
+curl -s -S --max-time 10 $MONITOR_URL >> /dev/null
+
+RESPONSE=`curl -s -S --max-time 30 $MONITOR_URL`
 
 if [[ -z `echo $RESPONSE | grep $PATTERN` ]]; then
     error "Monitor page '$MONITOR_URL' did not contain '$PATTERN'."
