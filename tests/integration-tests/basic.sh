5c36db9428cbff94c04e00da308947f6d4590bee
Patric Jansson <paddy@kth.se>
2017-12-01 14:45:09 +0100

    Runs redis in integration

--- tests/integration-tests/basic.sh
+++ tests/integration-tests/basic.sh
@@ -3,18 +3,19 @@
 error() { printf "\n \033[0;31mERROR: $@\033[0;0m | $(date) \n"; }
 passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
 
-MONITOR_URL="http://web:3000/_monitor";
-PATTERN="APPLICATION_STATUS"
+# /redis will connect to redis and say OK, if it works.
+MONITOR_URL="http://web:3000/redis";
+PATTERN="ok"
 
 sleep 5s
 
 RESPONSE=`curl -s -S --max-time 30 $MONITOR_URL`
 
 if [[ -z `echo $RESPONSE | grep $PATTERN` ]]; then
-    error "Monitor page '$MONITOR_URL' did not contain '$PATTERN'."
+    error "URL '$MONITOR_URL' does not contain '$PATTERN'."
     exit -1
 fi
 
-passed "Basic monitor test passed for '$MONITOR_URL'."
+passed "Basic test passed for '$MONITOR_URL'."
 
 exit 0
