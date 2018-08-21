b5358250c166c701fd9ca9d7903d67734d9485c3
Patric Jansson <paddy@kth.se>
2018-08-21 15:49:55 +0200

    bsaic

--- tests/integration-tests/basic.sh
+++ tests/integration-tests/basic.sh
@@ -4,14 +4,15 @@ error() { printf "\n \033[0;31mERROR: $@\033[0;0m | $(date) \n"; }
 passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
 
 # /redis will connect to redis and say OK, if it works.
-MONITOR_URL="http://web:3000/kth-azure-app/redis";
-PATTERN="ok"
+MONITOR_URL="http://web:3000/kth-azure-app/_monitor";
+PATTERN="APPLICATION_STATUS: OK"
 
 sleep 5s
 
 RESPONSE=`curl -s -S --max-time 30 $MONITOR_URL`
+echo "$RESPONSE"
 
-if [[ "$RESPONSE" != *"$PATTERN"* ]]; then
+if [[ $RESPONSE != *"$PATTERN"* ]]; then
     error "URL '$MONITOR_URL' does not contain '$PATTERN'."
     exit 1
 fi
