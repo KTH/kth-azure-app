0e450a327e4780b1685fd0374c61b7ace2682579
Patric Jansson <paddy@kth.se>
2018-08-21 15:23:38 +0200

    Intended failure

--- tests/integration-tests/basic.sh
+++ tests/integration-tests/basic.sh
@@ -5,17 +5,15 @@ passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
 
 # /redis will connect to redis and say OK, if it works.
 MONITOR_URL="http://web:3000/kth-azure-app/redis";
-PATTERN="ofailk"
+PATTERN="ok"
 
 sleep 5s
 
 RESPONSE=`curl -s -S --max-time 30 $MONITOR_URL`
 
-echo $RESPONSE
-
-if [[ "$RESPONSE" == *"$PATTERN"* ]]; then
+if [[ "$RESPONSE" != *"$PATTERN"* ]]; then
     error "URL '$MONITOR_URL' does not contain '$PATTERN'."
-    exit -1
+    exit 1
 fi
 
 passed "Basic test passed for '$MONITOR_URL'."
