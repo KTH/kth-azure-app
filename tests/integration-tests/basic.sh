4c2ffb0c00bcfe323505b3ad409f65c6969224f5
Patric Jansson <paddy@kth.se>
2018-08-22 11:16:11 +0200

    Env conf testing

--- tests/integration-tests/basic.sh
+++ tests/integration-tests/basic.sh
@@ -3,12 +3,12 @@
 error() { printf "\n \033[0;31mERROR: $@\033[0;0m | $(date) \n"; }
 passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
 
-#MONITOR_URL="http://web:3000/kth-azure-app/_monitor";
-MONITOR_URL="https://www.kth.se/_monitor";
+MONITOR_URL="http://web:3000/kth-azure-app/_monitor";
 
-PATTERN="APPLICATION_STATUS: OK"
+PATTERN="SECRET_VALUE_ON__MONITOR" #env in docker-compose-integration-tests.yml
 
 RESPONSE="$(curl -s -S --max-time 30 $MONITOR_URL)"
+echo "$RESPONSE"
 
 if [[ $RESPONSE != *$PATTERN* ]]; then
     error "URL '$MONITOR_URL' does not contain '$PATTERN'."
