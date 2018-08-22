61d7ffa6001557ae7715d69f6cd5c1b347821112
Patric Jansson <paddy@kth.se>
2018-08-22 13:19:12 +0200

    error

--- tests/integration-tests/basic.sh
+++ tests/integration-tests/basic.sh
@@ -5,7 +5,7 @@ passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
 
 MONITOR_URL="http://web:3000/kth-azure-app/_monitor";
 
-PATTERN="SECRET_VALUE_ON__MONdddITOR" #env in docker-compose-integration-tests.yml
+PATTERN="SECRET_VALUE_ON__MONddddITOR" #env in docker-compose-integration-tests.yml
 
 RESPONSE="$(curl -s -S --max-time 30 $MONITOR_URL)"
 echo "$RESPONSE"
