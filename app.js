bffc91c5d0750391dc8ce9a81d9602b16d7985c5
Patric Jansson <paddy@kth.se>
2019-03-04 14:29:03 +0100

    Välkommen björn

--- app.js
+++ app.js
@@ -192,6 +192,7 @@ app.get("/kth-azure-app/_monitor", function (req, res) {
   let msg = ``;
   if (process.env.ENV_TEST) {
     msg = `APPLICATION_STATUS: OK
+          HEJ Björn
           ENV_TEST: ${process.env.ENV_TEST}
               `;
   } else {
