ea13c62c80632716caafc222924f2de373e5156d
Patric Jansson <paddy@kth.se>
2020-04-16 00:32:03 +0200

    simpler e2e tests

--- tests/integration-tests/basic.sh
+++ tests/integration-tests/basic.sh
@@ -1,21 +1,66 @@
 #!/bin/bash
 
-error() { printf "\n \033[0;31mERROR: $@\033[0;0m | $(date) \n"; }
-passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
+info() { printf "\033[1;35m\n    • %s\033[0;0m$@\n";  }
+error() { printf "\033[0;31m    ERROR: $@\033[0;0m\n"; }
+passed() { printf "\033[0;32m   OK: $@\033[0;0m\n"; }
 
-MONITOR_URL="http://web:3000/kth-azure-app/_monitor";
 
-PATTERN="SECRET_VALUE_ON__MONITOR" #env in set in file: docker-compose-integration-tests.yml
+#
+# Path to the Cisco vpn client.
+#
+if [ -z "$URL_PREFIX" ]; then
+    URL_PREFIX="http://web:3000/kth-azure-app"
+    sleep 5s
+fi
 
-sleep 5s
+FAILED=""
 
-RESPONSE="$(curl -s -S --max-time 30 $MONITOR_URL)"
+#
+# Curls a url and tests if the response contains a string.
+# If it fails sets FAILED to true.
+#
+# Usage: expectPathToContain "/_monitor" "active"
+#
+expectPathToContain() {
+    
+    ENDPOINT="$1"
+    PATTERN="$2"
+    
+    TEST_URL="$URL_PREFIX$ENDPOINT"
 
-if [[ $RESPONSE != *$PATTERN* ]]; then
-    error "URL '$MONITOR_URL' does not contain '$PATTERN'."
+    curl -k -S --max-time 3 $TEST_URL > .curl.log 2>&1
+    RESULT=$(cat .curl.log)
+    
+    if [[ "$RESULT" == *"$PATTERN"* ]]; then
+        passed "$TEST_URL works, it contains $PATTERN." 
+    else
+        error "'$TEST_URL' does not contain pattern '$PATTERN'."
+        FAILED="true"
+    fi
+
+}
+
+# ---------------- Tests ----------------
+
+expectPathToContain "/" "Continuous Delivery Reference Application"
+expectPathToContain "/_monitor" "APPLICATION_STATUS: OK"
+expectPathToContain "/_monitor" "SECRET_VALUE_ON__MONITOR"
+expectPathToContain "/_monitor" "API Call: kth-azure-app"
+expectPathToContain "/_about" "Docker image"
+
+
+# Result
+if [[ "$FAILED" != *"true"* ]]; then
+    info "All end-to-end tests passed."
+    exit 0
+else
+    echo ""
     exit 1
 fi
 
-passed "Basic test passed for '$MONITOR_URL'."
-    
-exit 0
+
+
+
+
+
+
