4e689b07d88196f202ea2d1a8cca5b2bfed94b77
Patric Jansson <paddy@kth.se>
2020-04-16 17:52:03 +0200

    unit test fix

--- tests/unit-tests/test-app.js
+++ tests/unit-tests/test-app.js
@@ -3,47 +3,19 @@
 
 // Testing libraries
 const expect = require("chai").expect;
-const templates = require("../../modules/templates");
+const monitor = require("../../modules/monitor");
 
 describe("Template handling", function () {
-  it("Path '/' should contain a title.", function () {
-    const result = templates.index();
-    expect(result).to.contain("Continuous Delivery Reference Application");
-  });
-
-  it("Path '/error404' should contain a message.", function () {
-    const result = templates.error404();
-    expect(result).to.contain("404 - Page not found");
-  });
-
-  it("The header should contain env Application Insights key 'APPINSIGHTS_INSTRUMENTATIONKEY' if set.", function () {
-    process.env.APPINSIGHTS_INSTRUMENTATIONKEY = "abcd-1234-efghi";
-    const result0 = templates.index();
-    expect(result0).to.contain("abcd-1234-efghi");
-    const result1 = templates.error404();
-    expect(result1).to.contain("abcd-1234-efghi");
-    delete process.env.APPINSIGHTS_INSTRUMENTATIONKEY;
-  });
-
-  it("Path '/_monitor' should contain 'APPLICATION_STATUS: OK'.", async function () {
-    const result = await templates._monitor();
-    expect(result).to.contain("APPLICATION_STATUS: OK");
-  });
-
   it("Path '/_monitor' should contain ENV_TEST value specified in env 'ENV_TEST' if set.", async function () {
     process.env.ENV_TEST = "a value";
-    const result = await templates._monitor();
+    const result = await monitor.tests();
     expect(result).to.contain("a value");
     delete process.env.ENV_TEST;
   });
 
   it("Path '/_monitor' should contain 'No env value for ENV_TEST is set.' when env 'ENV_TEST' is not set.", async function () {
-    const result = await templates._monitor();
+    const result = await monitor.tests();
     expect(result).to.contain("No env value for ENV_TEST is set.");
-  });
-
-  it("Path '/502' should contain '502 Bad Gateway'.", function () {
-    const result = templates.badGateway();
-    expect(result).to.contain("502 Bad Gateway");
+    expect(result).to.contain("Could not read api.kth.se");
   });
 });
