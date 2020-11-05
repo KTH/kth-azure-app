54f90757b25d399f1d2ae8389aa27c04a304cd95
Patric Jansson <paddy@kth.se>
2020-11-05 16:57:09 +0100

    More fail

--- tests/unit-tests/test-app.js
+++ tests/unit-tests/test-app.js
@@ -19,9 +19,7 @@ describe("Template handling", function () {
   it("Path '/_monitor' should contain 'No env value for ENV_TEST is set.' when env 'ENV_TEST' is not set.", async function () {
     process.env.EXTERNAL_API_CALL = defaultEnvs.DEFAULTS.EXTERNAL_API_CALL;
     const result = await monitor.tests();
-    expect(result).to.contain(
-      "No env FAILJRE FALJASDF value for ENV_TEST is set"
-    );
+    expect(result).to.contain("No env FAILJRE FALJA value for ENV_TEST is set");
     expect(result).to.contain("API Call: kth-azure-app");
     delete process.env.EXTERNAL_API_CALL;
   });
