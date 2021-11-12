e2ea6e7b94e5300b384de0d6f8f15a2644215ccd
Patric Jansson <paddy@kth.se>
2021-11-12 21:52:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -111,4 +111,3 @@ app.get("/kth-azure-app/robots.txt", function (request, response) {
 app.use(function (request, response) {
   httpResponse.notFound(request, response, templates.error404());
 });
-
