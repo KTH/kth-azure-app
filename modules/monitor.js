a2b2e2f99d308d149cbc5c0c54dc4a049078e5c7
Patric Jansson <paddy@kth.se>
2022-03-14 22:47:09 +0100

    Refactoring

--- modules/monitor.js
+++ modules/monitor.js
@@ -9,16 +9,23 @@ const { statusCodes } = require("@kth/http-responses");
  */
 async function tests() {
   const text = `
-  - ENV_TEST: ${
-    process.env.ENV_TEST
-      ? process.env.ENV_TEST
-      : "No env value for ENV_TEST is set."
-  } - Should return the cluster name.
+  - ENV_TEST: ${getEnvTest()} - Should return the cluster name.
   - API Call: ${await api.getStatus()} - Should always return this applications name.
   `;
 
   logger.log.info(text);
   return text;
+}
+
+/**
+ * Gets the env ENV_TEST or a error message.
+ * This tests that ENV handling works in clusters.
+ * @returns The env ENV_TEST or a error message.
+ */
+function getEnvTest() {
+  return process.env.ENV_TEST
+    ? process.env.ENV_TEST
+    : "No env value for ENV_TEST is set.";
 }
 
 /**
