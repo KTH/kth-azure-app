9326355f33917bdcfeb3fd6a939b00660356d3cc
Mikael Bengtsson <62339114+mibengt@users.noreply.github.com>
2022-10-18 16:49:08 +0200

    my bump

--- app.js
+++ app.js
@@ -121,3 +121,4 @@ app.get("/kth-azure-app/robots.txt", function (request, response) {
 app.use(function (request, response) {
   httpResponse.notFound(request, response, templates.error404());
 });
+
